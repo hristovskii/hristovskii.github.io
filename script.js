708a4336f8ff1ef9636f3db00a3fac0f93280def
Petar Hristovski <petarhristovski23@gmail.com>
2023-07-07 01:15:49 +0200

    fixed navbar while responsive for phone

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick=()=>{
+menuIcon.onclick= () =>{
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
-}
+};
 
 
 let sections = document.querySelectorAll('section');
@@ -19,12 +19,12 @@ window.onscroll = () => {
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
-    if(top>= offset && top< offset + height){
+    if(top >= offset && top < offset + height){
       navLinks.forEach(links => {
         links.classList.remove('active');
         document.querySelector('header nav a[href*='+ id +']').classList.add('active');
       });
-    }
+    };
   } );
 
 
