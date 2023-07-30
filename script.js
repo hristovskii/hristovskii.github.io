70b48176ee3a542b5695b876f9f70d677ed6d74f
Petar Hristovski <petarhristovski23@gmail.com>
2023-07-30 15:04:33 +0200

    FTR: added popup modal to all "read more!" buttons

--- script.js
+++ script.js
@@ -82,3 +82,72 @@ let popup = document.getElementById("popup");
         function closePopup(){
             popup.classList.remove("open-popup");
         }
+
+let popup1 = document.getElementById("popup1");
+
+        function openPopup1(){
+            popup1.classList.add("open-popup1");
+        }
+        function closePopup1(){
+            popup1.classList.remove("open-popup1");
+        }
+
+
+let popup2 = document.getElementById("popup2");
+
+        function openPopup2(){
+            popup2.classList.add("open-popup2");
+        }
+        function closePopup2(){
+            popup2.classList.remove("open-popup2");
+        }
+
+
+let popup3 = document.getElementById("popup3");
+
+        function openPopup3(){
+            popup3.classList.add("open-popup3");
+        }
+        function closePopup3(){
+            popup3.classList.remove("open-popup3");
+        }
+
+
+let popup4 = document.getElementById("popup4");
+
+        function openPopup4(){
+            popup4.classList.add("open-popup4");
+        }
+        function closePopup4(){
+            popup4.classList.remove("open-popup4");
+        }
+
+
+let popup5 = document.getElementById("popup5");
+
+        function openPopup5(){
+            popup5.classList.add("open-popup5");
+        }
+        function closePopup5(){
+            popup5.classList.remove("open-popup5");
+        }
+
+
+let popup6 = document.getElementById("popup6");
+
+        function openPopup6(){
+            popup6.classList.add("open-popup6");
+        }
+        function closePopup6(){
+            popup6.classList.remove("open-popup6");
+        }
+
+let popup7 = document.getElementById("popup7");
+
+        function openPopup7(){
+            popup7.classList.add("open-popup7");
+        }
+        function closePopup7(){
+            popup7.classList.remove("open-popup7");
+        }
+
