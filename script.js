21a4676ab6f869162bb7d7d87380ce1a41bcfcc6
Petar Hristovski <petarhristovski23@gmail.com>
2024-05-31 01:21:07 +0200

    fixing the functions for popups

--- script.js
+++ script.js
@@ -77,73 +77,12 @@ ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
 
 
-let popup = document.getElementById("popup");
-
-        function openPopup(){
-            popup.classList.add("open-popup");
-        }
-        function closePopup(){
-            popup.classList.remove("open-popup");
-        }
-
-let popup1 = document.getElementById("popup1");
-
-        function openPopup1(){
-            popup1.classList.add("open-popup1");
-        }
-        function closePopup1(){
-            popup1.classList.remove("open-popup1");
-        }
-
-
-let popup2 = document.getElementById("popup2");
-
-        function openPopup2(){
-            popup2.classList.add("open-popup2");
-        }
-        function closePopup2(){
-            popup2.classList.remove("open-popup2");
-        }
-
-
-let popup3 = document.getElementById("popup3");
-
-        function openPopup3(){
-            popup3.classList.add("open-popup3");
-        }
-        function closePopup3(){
-            popup3.classList.remove("open-popup3");
-        }
-
-
-let popup4 = document.getElementById("popup4");
-
-        function openPopup4(){
-            popup4.classList.add("open-popup4");
-        }
-        function closePopup4(){
-            popup4.classList.remove("open-popup4");
-        }
-
-
-let popup5 = document.getElementById("popup5");
-
-        function openPopup5(){
-            popup5.classList.add("open-popup5");
-        }
-        function closePopup5(){
-            popup5.classList.remove("open-popup5");
-        }
-
-
-let popup6 = document.getElementById("popup6");
-
-        function openPopup6(){
-            popup6.classList.add("open-popup6");
-        }
-        function closePopup6(){
-            popup6.classList.remove("open-popup6");
-        }
+function openPopup(i) {
+  document.getElementById("popup"+i).classList.add("open-popup"+i);
+}
+function closePopup(i) {
+document.getElementById("popup"+i).classList.remove("open-popup"+i);
+}
 
 let popup7 = document.getElementById("popup7");
 
